99158b15c9e596b6b063d08c228ca27efbf1eb70
demetriusleonardobantim <demetriusbantimvas@gmail.com>
2024-02-02 15:12:40 -0300

    first commit

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,18 @@
+import { PrimaryGeneratedColumn, Column } from 'typeorm';
+
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column()
+  image_url: string;
+
+  @Column()
+  price: string;
+}
